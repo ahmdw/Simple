5831086b3fd9ffea934e17449cd57f9a9a1f0016
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2022-07-28 15:08:27 +0300

    Updates

---
+++ precache-manifest.42511e44808eb23da68dbd53ed4b3e11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2db246bb57dca154abc",
+    "url": "/Simple/static/css/main.5e4127f9.chunk.css"
+  },
+  {
+    "revision": "a2db246bb57dca154abc",
+    "url": "/Simple/static/js/main.a2db246b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Simple/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/Simple/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "c2cccb2730114c2857c4",
+    "url": "/Simple/static/js/runtime~main.c2cccb27.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Simple/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c86bf55781b962ae6dcdd0973b106a23",
+    "url": "/Simple/index.html"
+  }
+];
